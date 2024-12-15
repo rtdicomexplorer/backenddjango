56c131dddc78fcbe1c2677e10807c142d9af1ef0
Michele Bufano <michele.bufano@ad.uniklinik-freiburg.de>
2024-12-15 20:38:41 +0100

    added .env parameters to settings.py

--- baseproject/settings.py
+++ baseproject/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 from datetime import timedelta
-
+load_dotenv(override=True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
