70748c58e76de7fba82019e6a13bb0b5b9e8fb4f
Michele Bufano <michele.bufano@ad.uniklinik-freiburg.de>
2024-12-15 19:36:44 +0100

    created custom users

--- api/models.py
+++ api/models.py
@@ -1,3 +1,64 @@
+from django.contrib.auth.models import AbstractBaseUser, UserManager
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("You have not provided a valid e-mail address")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('image', "avatars/user.png")
+        return self._create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser):
+    username = None
+    image = models.ImageField(upload_to = 'avatars/', null=True, blank=True)
+    email = models.EmailField(blank=True, default='', unique=True)
+    name = models.CharField(max_length=255, blank=True, default='')
+
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(blank=True, null=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []   
+
+    class Meta:
+        verbose_name = 'CustomUser'
+        verbose_name_plural = 'CustomUsers'
+    
+    def get_full_name(self):
+        return self.name
+    
+    def get_short_name(self):
+        return self.name or self.email
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
